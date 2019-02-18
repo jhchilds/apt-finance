61c9d94f2b41a3f6f0893ae877ada6f1c38c27ca
joshuachilds <jhchilds@uvm.edu>
2019-02-18 01:12:38 -0500

    editing form

--- form.php
+++ form.php
@@ -305,7 +305,7 @@ if (isset($_POST["btnSubmit"])) {
                 $message .= $oneWord . ' ';
             }
 
-            $message .= ' = ' . htmlentities($value, ENT_QUOTES, "UTF-8") . '</p>';
+            $message .= ' = ' . htmlentities(number_format($value), ENT_QUOTES, "UTF-8") . '</p>';
         // mailing information
         }
         $to = $email;
